d2869e02aa10dd2b257628845eabc0035b246b53
zagorky <zagorkiynahyi@gmail.com>
2024-03-14 21:28:44 +0300

    Решение пятой задачи

---
+++ js/arrays5.js
@@ -0,0 +1,14 @@
+// Написать функцию, которая принимает массив 
+// чисел и возвращает среднее арифметическое, 
+// (округлить результат до десятых). 
+// (reduce для суммы)
+
+let n = Math.round(Math.random()*3+1);
+let arr =  new Array();
+for(let i=0; i<n; i++){
+    arr[i] = ((Math.round(Math.random()*100) * 10) / 100);
+}
+let checkMiddle = ( arr.reduce((sum, i) => sum + i) / arr.length).toFixed(1) ;
+document.write(arr);
+document.write (' => ');
+document.write(checkMiddle);
