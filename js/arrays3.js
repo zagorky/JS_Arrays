6808fc768a4f8c3e94a72d283e67d3dd9d46d488
zagorky <zagorkiynahyi@gmail.com>
2024-03-14 20:14:24 +0300

    Выполнена третья задача

---
+++ js/arrays3.js
@@ -0,0 +1,16 @@
+// Написать функцию, которая принимает массив
+// любых целых чисел и возвращает истину, 
+// если все элементы четные. Если имеется хотя
+//  бы один элемент не четный, то false. (every)
+
+let n = Math.round(Math.random()*5+1);
+let arr =  new Array();
+for(let i=0; i<n; i++){
+    arr[i] = Math.round(Math.random()*100);
+}
+function checkEvenNum (elem, i, arr){
+    return elem%2==0;
+}
+let evenNum = arr.every(checkEvenNum);
+document.write(arr);
+document.write(evenNum);
