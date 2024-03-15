6cb2ca9df36a9707455c23ac28987ce7691dfe06
zagorky <zagorkiynahyi@gmail.com>
2024-03-15 20:38:35 +0300

    Решение восьмой задачи

--- js/arrays8.js
+++ js/arrays8.js
@@ -3,3 +3,11 @@
 // которого больше на единицу. Если попалась 
 // цифра 9 она также заменяется на 10. 
 // (split, map)
+
+let num = prompt('Введите ЧИСЛО');
+if (Boolean(num)){
+let arrNum = num.split('');
+let res = arrNum.map(item => +item+1);
+if (isNaN(res)) alert('ОШИБКА')
+else document.write(res); }
+else alert('ОШИБКА');
