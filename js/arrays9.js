845dc5385a88e0767191b87871dab049072e9be2
zagorky <zagorkiynahyi@gmail.com>
2024-03-16 13:52:55 +0300

    проверка

--- js/arrays9.js
+++ js/arrays9.js
@@ -3,3 +3,24 @@
 // на http://.(filter,startsWith)
 // Используйте возможности объекта Object 
 // (.keys, .values,.entries)
+
+// function findData (arr){
+    //    return arr.filter((elem) => elem == 'http://')
+    // return elem.startsWith('http://');
+    // }
+
+let data = {
+    link : 'https://www.youtube.com/' ,
+    name : 'YouTube',
+    discription : 'video hosting',
+};
+let arrData = Object.values(data);
+
+
+// let thisData = '0';
+// let arrData = Object.values(data);
+
+// let res = findData(arrData );
+console.log(data);
+console.log(arrData);
+console.log();
