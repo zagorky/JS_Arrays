9ba4b6541a00447fbe701ca3d69b633909ae0068
zagorky <zagorkiynahyi@gmail.com>
2024-03-16 13:39:27 +0300

    Решение десятой задачи

--- js/arrays10.js
+++ js/arrays10.js
@@ -1,21 +1,29 @@
 // Дан объект с ключами и значениями. 
 // Запишите в первый массив ключи объекта, 
 // а во второй - значения.
-// let data = [
-// {
-// 1: 11,
-// 2: 12,
-// 3: 13,
-// },
-// {
-// 1: 21,
-// 2: 22,
-// 3: 23,
-// },
-// {
-// 1: 24,
-// 2: 25,
-// 3: 26,
-// },
-// ];
 // Найдите сумму элементов этой структуры.
+
+let data = [
+    {
+    1: 11,
+    2: 12,
+    3: 13,
+    },
+    {
+    1: 21,
+    2: 22,
+    3: 23,
+    },
+    {
+    1: 24,
+    2: 25,
+    3: 26,
+    },
+    ];
+let keys = data.reduce((cont,obj) => [...cont, ...Object.keys(obj)], [])
+
+console.log(keys);
+let values = data.reduce((cont, obj) => [...cont, ...Object.values(obj)], [] )
+console.log(values);
+let sum = keys.reduce((sum,i) => sum + +i, 0) + values.reduce((sum,i) => sum + +i, 0);
+console.log(sum);
