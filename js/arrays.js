d4dcbc6b9821943301af72c415ac08ad2451185d
Daria <156232667+zagorky@users.noreply.github.com>
2024-03-20 20:06:24 +0300

    Update arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -7,6 +7,7 @@ let arr =  new Array();
 for(let i=0; i<n; i++){
     arr[i] = Math.round(Math.random()*100);
 }
-console.log(arr);
+document.write(arr);
 let sumSqrt = arr.filter(i => i % 2==0).reduce ( (sum,i) => sum + (i ** 0.5) , 0);
+document.write('=>');
 console.log(sumSqrt);
